9e06d3e85cb7f8ddcaf4ce6e42c64be2ff5c57d2
Tùng Trần <tungtt09605@gmail.com>
2021-10-17 21:03:59 +0700

    cmt getListLead 9:05/10/17/2021

--- src/main/java/shupship/controller/LeadController.java
+++ src/main/java/shupship/controller/LeadController.java
@@ -1,33 +1,15 @@
-<<<<<<< HEAD
 package shupship.controller;
 
-import org.hibernate.collection.internal.PersistentList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import shupship.domain.dto.response.lead.PagingRs;
-import shupship.domain.model.Lead;
 import shupship.service.ILeadService;
 import shupship.util.exception.ApplicationException;
-
-import java.util.ArrayList;
-import java.util.List;
-
-=======
-
-package shupship.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import shupship.service.ILeadService;
-import shupship.util.exception.ApplicationException;
 
->>>>>>> origin/dev_tungtt
 @RestController
 @RequestMapping(value = "api/lead")
 public class LeadController extends BaseController {
