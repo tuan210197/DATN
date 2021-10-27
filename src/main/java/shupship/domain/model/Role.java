4d60ec0b0fc7115704f7eb03d86177106ee7c9a1
tuan <doanvantuan197bg@gmail.com>
2021-10-27 09:42:21 +0700

    ok

---
+++ src/main/java/shupship/domain/model/Role.java
@@ -0,0 +1,28 @@
+package shupship.domain.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.*;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "role")
+@Table(name = "role")
+@Data
+@NoArgsConstructor
+public class Role implements Serializable{
+    private static final long serialVersionUID = -5204391003825277886L;
+
+    @Id  @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<Users> users = new ArrayList<>();
+
+
+}
